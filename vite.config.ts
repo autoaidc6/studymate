d1eec2de592c94b8d85739b34b011a888c36054a
autoaidc6 <autoaidc6@gmail.com>
2025-10-26 05:44:55 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        preview.allowedHosts: 'studymate-ewb5.onrender.com'
       },
       plugins: [react()],
       define: {
